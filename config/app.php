7ca7d3c5f112eae49f1077883f04a9c1c56a6eeb
unknown <itopfajar@gmail.com>
2026-02-02 11:40:09 +0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Sistem Aset dan Tata Kelola BPN DIY'),
 
     /*
     |--------------------------------------------------------------------------
